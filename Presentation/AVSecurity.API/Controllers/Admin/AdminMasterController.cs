e2130ca515e30716db6479504e0581ec996ced79
sujayjena <sujay930@gmail.com>
2024-06-29 22:47:57 +0530

    worked on api

--- Presentation/AVSecurity.API/Controllers/Admin/AdminMasterController.cs
+++ Presentation/AVSecurity.API/Controllers/Admin/AdminMasterController.cs
@@ -683,56 +683,56 @@ namespace AVSecurity.API.Controllers.Admin
 
         #region MeritalStatus
 
-        //[Route("[action]")]
-        //[HttpPost]
-        //public async Task<ResponseModel> SaveMeritalStatus(MeritalStatus_Request parameters)
-        //{
-        //    int result = await _adminMasterRepository.SaveMeritalStatus(parameters);
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<ResponseModel> SaveMeritalStatus(MeritalStatus_Request parameters)
+        {
+            int result = await _adminMasterRepository.SaveMeritalStatus(parameters);
 
-        //    if (result == (int)SaveOperationEnums.NoRecordExists)
-        //    {
-        //        _response.Message = "No record exists";
-        //    }
-        //    else if (result == (int)SaveOperationEnums.ReocrdExists)
-        //    {
-        //        _response.Message = "Record is already exists";
-        //    }
-        //    else if (result == (int)SaveOperationEnums.NoResult)
-        //    {
-        //        _response.Message = "Something went wrong, please try again";
-        //    }
-        //    else
-        //    {
-        //        _response.Message = "Record details saved sucessfully";
-        //    }
-        //    return _response;
-        //}
+            if (result == (int)SaveOperationEnums.NoRecordExists)
+            {
+                _response.Message = "No record exists";
+            }
+            else if (result == (int)SaveOperationEnums.ReocrdExists)
+            {
+                _response.Message = "Record is already exists";
+            }
+            else if (result == (int)SaveOperationEnums.NoResult)
+            {
+                _response.Message = "Something went wrong, please try again";
+            }
+            else
+            {
+                _response.Message = "Record details saved sucessfully";
+            }
+            return _response;
+        }
 
-        //[Route("[action]")]
-        //[HttpPost]
-        //public async Task<ResponseModel> GetMeritalStatusList(MeritalStatus_Search parameters)
-        //{
-        //    IEnumerable<MeritalStatus_Response> lstRoles = await _adminMasterRepository.GetMeritalStatusList(parameters);
-        //    _response.Data = lstRoles.ToList();
-        //    _response.Total = parameters.Total;
-        //    return _response;
-        //}
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<ResponseModel> GetMeritalStatusList(MeritalStatus_Search parameters)
+        {
+            IEnumerable<MeritalStatus_Response> lstRoles = await _adminMasterRepository.GetMeritalStatusList(parameters);
+            _response.Data = lstRoles.ToList();
+            _response.Total = parameters.Total;
+            return _response;
+        }
 
-        //[Route("[action]")]
-        //[HttpPost]
-        //public async Task<ResponseModel> GetMeritalStatusById(int Id)
-        //{
-        //    if (Id <= 0)
-        //    {
-        //        _response.Message = "Id is required";
-        //    }
-        //    else
-        //    {
-        //        var vResultObj = await _adminMasterRepository.GetMeritalStatusById(Id);
-        //        _response.Data = vResultObj;
-        //    }
-        //    return _response;
-        //}
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<ResponseModel> GetMeritalStatusById(int Id)
+        {
+            if (Id <= 0)
+            {
+                _response.Message = "Id is required";
+            }
+            else
+            {
+                var vResultObj = await _adminMasterRepository.GetMeritalStatusById(Id);
+                _response.Data = vResultObj;
+            }
+            return _response;
+        }
 
         #endregion
     }
